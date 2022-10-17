840cfa311185c5f5caaf48a038ea4376a2936605
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-10-17 12:27:27 +0300

    Update and rename Sum of Digits  Digital Root.py.txt to Sum of Digits \Digital Root.py

--- 6 Kyu/Sum of Digits \Digital Root.py
+++ 6 Kyu/Sum of Digits \Digital Root.py
@@ -1,14 +1,12 @@
-# Description
+# Description:
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
 
-Digital root is the recursive sum of all the digits in a number.
-
-Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
-
-Examples
-    16  -->  1 + 6 = 7
-   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
-132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
-493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+# Examples
+#     16  -->  1 + 6 = 7
+#    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+# 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 
 
 # My solution:
@@ -41,10 +39,10 @@ print(digital_root(493193))
 
 
 
-# Best practice solution:
+# Best practice solutions:
 
 # def digital_root(n):
-    return n if n < 10 else digital_root(sum(map(int,str(n))))
+#     return n if n < 10 else digital_root(sum(map(int,str(n))))
 
 # def digital_root(n):
-    return n%9 or n and 9 
+#     return n%9 or n and 9 
