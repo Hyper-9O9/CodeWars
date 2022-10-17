c3b2813bc582b51860c2d1fa3e361896fff497a8
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-10-17 10:19:38 +0300

    Create You Can't Code Under Pressure.py

---
+++ 8 Kyu/You Can't Code Under Pressure.py
@@ -0,0 +1,11 @@
+# Description:
+# Code as fast as you can! You need to double the integer and return it.
+
+# My solution
+
+def double_integer(i):
+    return i * 2
+
+
+
+print(double_integer(5))
