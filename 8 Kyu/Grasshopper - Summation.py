56e64f4c23805a6764efff40d9a4a02af6c40156
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-10-17 10:27:59 +0300

    Create Grasshopper - Summation.py

---
+++ 8 Kyu/Grasshopper - Summation.py
@@ -0,0 +1,32 @@
+# Description
+
+# Summation
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+# For example:
+
+# summation(2) -> 3
+# 1 + 2
+
+# summation(8) -> 36
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+
+# My solution
+
+def summation(num):
+    running = False
+    sum = 0
+    while not running:
+        if num == 0:
+            running = True
+            
+        sum += num
+        num -= 1
+    return sum
+
+
+
+print(summation(8))
+    
