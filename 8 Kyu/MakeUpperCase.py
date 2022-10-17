9d4f2037108d41ad2159118b0bea90edca8e2820
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-10-17 10:20:41 +0300

    Create MakeUpperCase.py

---
+++ 8 Kyu/MakeUpperCase.py
@@ -0,0 +1,12 @@
+# Description:
+# Write a function which converts the input string to uppercase.
+
+
+
+# My solution
+
+def make_upper_case(s):
+    upper_case = s.upper()
+    return upper_case
+
+print(make_upper_case('abcdefghijklmnopqrstuvwxyz'))
