757c10db78e1b49899d6050c41d8a79b424b37d5
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-10-17 10:26:13 +0300

    Create Are You Playing Banjo.py

---
+++ 8 Kyu/Are You Playing Banjo.py
@@ -0,0 +1,24 @@
+# Description
+
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+# The function takes a name as its only argument, and returns one of the following strings:
+
+# name + " plays banjo" 
+# name + " does not play banjo"
+# Names given are always valid strings.
+
+
+
+# My solution
+
+def are_you_playing_banjo(name):
+    if name.startswith('r') or name.startswith('R'):
+    
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+    
+print(are_you_playing_banjo("jack"))
+print(are_you_playing_banjo('rhino'))
